00b095e2d0776d5c2b669dd7666fffa67c5addce
TheOguzhan <ozkiroguzhan@gmail.com>
2021-05-18 18:25:36 +0300

    asd

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,39 +6,39 @@ interface IAppProps {
 
 const Table: React.FunctionComponent<IAppProps> = (props) => {
     return (
-        <table className="table-fixed mx-auto border-separate border border-green-800  my-8">
+        <table className="table-fixed mx-auto border-separate border dark:border-green-800 border-blue-800 my-8 dark:text-white text-dark">
             <thead>
                 <tr>
-                    <th className=" border-green-600 bg-green-600 p-2">#</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Command</th>
-                    <th className=" border-green-600 bg-green-600 p-2">p1</th>
-                    <th className=" border-green-600 bg-green-600 p-2">p2</th>
-                    <th className=" border-green-600 bg-green-600 p-2">p3</th>
-                    <th className=" border-green-600 bg-green-600 p-2">p4</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Lat</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Long</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Alt</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Frame</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Delete</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Up</th>
-                    <th className=" border-green-600 bg-green-600 p-2">Down</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">#</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Command</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">p1</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">p2</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">p3</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">p4</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Lat</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Long</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Alt</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Frame</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Delete</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Up</th>
+                    <th className=" dark:border-green-600 dark:bg-green-600 p-2 border-blue-500 bg-blue-500">Down</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td className=" border-green-600 bg-green-500 p-2">1</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Command</td>
-                    <td className=" border-green-600 bg-green-500 p-2">p1</td>
-                    <td className=" border-green-600 bg-green-500 p-2">p2</td>
-                    <td className=" border-green-600 bg-green-500 p-2">p3</td>
-                    <td className=" border-green-600 bg-green-500 p-2">p4</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Lat</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Long</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Alt</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Frame</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Delete</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Up</td>
-                    <td className=" border-green-600 bg-green-500 p-2">Down</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">1</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Command</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">p1</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">p2</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">p3</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">p4</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Lat</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Long</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Alt</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Frame</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Delete</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Up</td>
+                    <td className=" dark:border-green-600 dark:bg-green-500 p-2 border-blue-500 bg-blue-400">Down</td>
                 </tr>
             </tbody>
         </table>
---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+const config=
+{
+  REACT_APP_GOOGLE_MAPS_API_KEY: "AIzaSyCJiFmxscERxTx27kH19xycX0UJhKnmRXM"
+};
+
+export default config;
+
