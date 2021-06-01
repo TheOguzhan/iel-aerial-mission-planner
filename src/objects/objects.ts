456b51bccd4e8b92dca7c4a8e2b827ba112ec6ac
TheOguzhan <ozkiroguzhan@gmail.com>
2021-06-01 12:10:44 +0300

    English lesson is boring af

---
+++ src/objects/objects.ts
@@ -0,0 +1,15 @@
+const FrameObject: FrameObjectType = {
+    RELATIVE: 3,
+    ABSOLUTE: 0,
+    TERRAIN: 10
+};
+
+const FrameArray = ["RELATIVE", "ABSOLUTE", "TERRAIN"];
+
+const CommandArray: Array<String> = [];
+
+export {
+    FrameObject,
+    FrameArray,
+    CommandArray
+};
