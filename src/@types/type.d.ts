ea55fe75784ff86f5b933c5d52b8675da55114da
TheOguzhan <ozkiroguzhan@gmail.com>
2021-06-02 23:33:57 +0300

    working with commands

--- src/@types/type.d.ts
+++ src/@types/type.d.ts
@@ -1,13 +1,13 @@
 type Data = {
-    command:    String,
-    p1:         String,
-    p2:         String,
-    p3:         String,
-    p4:         String,
-    lat:        String,
-    long:       String,
-    alt:        String,
-    frame:      String
+    command: String,
+    p1: String,
+    p2: String,
+    p3: String,
+    p4: String,
+    lat: String,
+    long: String,
+    alt: String,
+    frame: String
 }
 
 type FrameObjectType = {
@@ -22,13 +22,21 @@ type CommandType = {
     p2: Boolean,
     p3: Boolean,
     p4: Boolean,
-    p1Name: String |undefined,
-    p2Name: String | undefined,
-    p3Name: String | undefined,
-    p4Name: String | undefined,
+    p1Payload?: String = Number(0).toFixed(8),
+    p2Payload?: String = Number(0).toFixed(8),
+    p3Payload?: String = Number(0).toFixed(8),
+    p4Payload?: String = Number(0).toFixed(8),
+    p1Name?: String,
+    p2Name?: String,
+    p3Name?: String,
+    p4Name?: String,
     latRequired: Boolean,
     longRequired: Boolean,
-    
+    altRequired: Boolean,
+    latPayload?: String = Number(0).toFixed(8),
+    longPayload?: String = Number(0).toFixed(8),
+    altPayload?: String = Number(100).toFixed(8),
+    payload: Number
 }
 
 type Center = {
--- src/objects/objects.ts
+++ src/objects/objects.ts
@@ -39,8 +39,46 @@ const CommandArray: Array<String> = [
     "UNKNOWN"
 ];
 
+const WAYPOINT_COMMAND: CommandType = {
+    Command: "WAYPOINT",
+    p1: true,
+    p2: false,
+    p3: false,
+    p4: false,
+    p1Name: "Delay",
+    latRequired: true,
+    longRequired: true,
+    altRequired: true,
+    payload: 16
+}
+
+const HOME_COMMAND: CommandType = {
+    Command: "HOME_POINT",
+    p1: true,
+    p2: false,
+    p3: false,
+    p4: false,
+    p1Name: "Delay",
+    latRequired: true,
+    longRequired: true,
+    altRequired: true,
+    payload: 16,
+    p1Payload: Number(0).toFixed(6),
+    p2Payload: Number(0).toFixed(6),
+    p3Payload: Number(0).toFixed(6),
+    p4Payload: Number(0).toFixed(6),
+}
+
+
+
+const COMMAND_TYPE_ARRAY: Array<CommandType> = [
+    WAYPOINT_COMMAND,
+    HOME_COMMAND
+]
+
 export {
     FrameObject,
     FrameArray,
-    CommandArray
+    CommandArray,
+    COMMAND_TYPE_ARRAY
 };
