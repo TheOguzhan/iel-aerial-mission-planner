5f3c03f7d4e53b071c7566bf7dc17292409de032
TheOguzhan <ozkiroguzhan@gmail.com>
2021-06-22 20:07:33 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Screen from "./screens/index";
-
+// new PC git
 const App: React.FC = () => {
   return (
     <div className="bg-gray-50 dark:bg-gray-900  pt-8 m-0  text-gray-900 dark:text-white max-w-full min-h-screen max-h-full h-full">
